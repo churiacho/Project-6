a430342371b29ac0d7441a6ba3bae6fe43f148d0
Julia Jo <julia.ohyoon@gmail.com>
2020-01-21 13:11:06 -0500

    added username to user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, except: :create, path: '', path_names: { sign_in: 'login', sign_out: 'signed-out' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :activities
-  root to: 'pages#home'
+  root 'pages#home'
   get 'signed-out', to: 'signouts#index'
 end
