11cedddd015009560d9fb6b95c5dc87ae3d7843d
Julia Jo <julia.ohyoon@gmail.com>
2020-01-23 15:49:21 -0500

    rails g devise:controllers, configured puma port to 3002, optimized nav bar and signin/signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,27 @@ class User < ApplicationRecord
   # def login=(str)
   #   @login = str
   # end
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 
   has_many :activities
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :registerable, :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable, :registerable
 
   def self.find_for_database_authentication warden_condition
     conditions = warden_condition.dup
-    login = conditions.delete(:login)
-    where(conditions).where(
-      ["lower(username) = :value OR lower(email) = :value",
-    { value: login.strip.downcase}]).first
+    if login = conditions.delete(:login).downcase
+      where(conditions).where('$or' => [ {:username => /^#{Regexp.escape(login)}$/i}, {:email => /^#{Regexp.escape(login)}$/i} ]).first
+    else
+      where(conditions).first
+      # ["lower(username) = :value OR lower(email) = :value",
+    # { value: login.strip.downcase}]).first
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    get "/sign_up" => "devise/registrations#new", as: "new_user_registration"
-  end
-  devise_for :users, except: :create, path: '', path_names: { sign_in: 'login', sign_out: 'signed-out' }
+
+  # devise_scope :user do
+  #   get "register" => "devise/registrations#new", as: "new_user_registration"
+  # end
+
+  devise_for :users, path: '', path_names: { sign_out: 'signed-out' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :activities
-  root 'pages#home'
+  root to: 'pages#home'
   get 'signed-out', to: 'signouts#index'
+  
 end
