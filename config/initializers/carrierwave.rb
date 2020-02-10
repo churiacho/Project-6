51e7f2983cc8cee63735191b92811c55e260e5a1
Julia Jo <julia.ohyoon@gmail.com>
2020-02-10 11:27:06 -0500

    added .env file with confidential info, dotenv gem, configuration for sendgrid

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
         provider:                   'AWS',
-        aws_access_key_id:          'AKIA4JTBOULHBTJRWROT',
-        aws_secret_access_key:      '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',
+        aws_access_key_id:          'ENV["AWS_ACCESS_KEY_ID"]',
+        aws_secret_access_key:      'ENV["AWS_SECRET_ACCESS_KEY"]',
         region:                     'ca-central-1'
     }
 
