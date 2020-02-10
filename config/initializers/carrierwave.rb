e9f96146564e1174dbfd55da027f44d3688a2d85
Julia Jo <julia.ohyoon@gmail.com>
2020-02-10 15:51:40 -0500

    removed fog.rb, changed carrierwave initializer config for aws-sdk instead of fog-aws, changed names from .env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-        provider:                   'AWS',
-        aws_access_key_id:          'ENV["AWS_ACCESS_KEY_ID"]',
-        aws_secret_access_key:      'ENV["AWS_SECRET_ACCESS_KEY"]',
-        region:                     'ca-central-1'
-    }
+    config.storage      = :aws
+    config.aws_bucket   = ENV['S3_BUCKET_NAME']
+    config.aws_acl      = 'public-read'
 
-    config.fog_directory    = 'moveandgroove'
-    config.fog_public       = false
-    config.fog_attributes   = { cache_control: "public, max-age=#{365.days.to_i}" }
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+
+    config.aws_credentials = {
+        access_key_id:      "ENV['AWS_ACCESS_KEY_ID']",
+        secret_access_key:  "ENV['AWS_SECRET_ACCESS_KEY']",
+        region:             "ENV['AWS_REGION']"
+    }
 end
