6e593febcd7ac4ffc5fc658246d74c813a8e4ed0
Julia Jo <julia.ohyoon@gmail.com>
2020-02-18 14:45:56 -0500

    deleted comments and files re: carrierwave

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
 
   has_many :activities , dependent: :destroy
   has_one_attached :avatar
-  # mount_uploader :avatar, AvatarUploader
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :registerable, :omniauthable
