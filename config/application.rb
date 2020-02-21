c9782e1b9921376fb160f369b935e48ca58df697
Julia Jo <julia.ohyoon@gmail.com>
2020-02-21 11:07:30 -0500

    assets.initialize on precompile to false

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module Moveandgroove
     config.load_defaults 6.0
     config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
+    config.assets.initialize_on_precompile = false
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
